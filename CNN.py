8858aed837f4d3921a237cb70f75bf88d326f475
josh2397 <josh23797@gmail.com>
2017-07-18 20:40:01 +1000

    Added feedForward and trainNetwork functions and other functions still to be completed.

--- CNN.py
+++ CNN.py
@@ -25,10 +25,44 @@ class convolutionalNeuralNetwork:
         self.shape = shape
 
         self.numberOfLayers = len(shape)
-
+        '''
+        setup weights for convolution architecture including max-pooling and fully connected layers
+        '''
         self.weights = [np.random.normal(0, 1 / np.sqrt(shape[i + 1]), (shape[i], shape[i + 1])) for i in range(self.numberOfLayers - 1)]
 
         self.bias = [np.random.normal(0, 1, (shape[i])) for i in range(1, self.numberOfLayers)]
 
+    def feedForward(self, inputdata):
+
+        self.layerInput = []
+
+        self.layerOutput = {}
+
+        self.layerInput[0] = inputdata
+
+        self.layerOutput = np.array(inputdata)
+
+        for layer in range(1, self.numberOfLayers):
+            self.layerInput[layer] += np.dot(self.layerOutput[layer - 1], self.weights[layer - 1] + self.bias[layer - 1])
+
+            self.layerOutput[layer] = np.array(sigmoid(self.layerInput[layer]))
+
+            return self.layerOutput[self.numberOfLayers - 1]
+
+    def trainNetwork(self, data, miniBatchSize, numberOfEpochs, earningRate):
+
+        #TODO - open data files
+
+        for epoch in np.arange(numberOfEpochs):
+
+    def storeWeightsAndBias(self):
+        #store the weights and bias in their separate respective files
+
+    def miniBatch(self):
+        #separate the data into a batch and execute feedforward followed by backpropagation
+
+
+
 
 network = convolutionalNeuralNetwork( [4096, 300, 250, 200, 150, 100, 50, 10] )
+#call trainNetwork as main function
